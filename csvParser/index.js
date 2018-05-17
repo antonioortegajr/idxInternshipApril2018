af491398a99a563bb647253e8526dc6198b41460
KadonDEngle <englekadondax@gmail.com>
2018-05-17 16:06:16 -0700

    Commented Functions

--- csvParser/index.js
+++ csvParser/index.js
@@ -3,9 +3,12 @@ var fs = require("fs");
 var rawData = '';
 var data = [];
 var csvContent = '';
+//Change this to your rest endpoint.
+var url = 'yourRestEndpoint';
 
+//Gets your rest endpoint and sorts into an array of arrays
 async function apiCall() {
-  await axios.get('sampleEndPoint')
+  await axios.get(url)
     .then(function (response) {
       rawData = response.data;
     })
@@ -25,6 +28,7 @@ async function apiCall() {
   return data;
 }
 
+//Takes arrays from apiCall function and changes them into csv format
 async function formatData() {
   var input = await apiCall();
   const header = ['Name', 'Email', 'Converted', 'Date/Time']
@@ -36,6 +40,7 @@ async function formatData() {
   return header + '\r\n' + csvContent;
 }
 
+//Takes csv formatted data and creates a csv file in the same directory
 async function createFile(fileName) {
   var csvData = await formatData();
   await fs.appendFile(fileName + '.csv', csvData, function (err) {
