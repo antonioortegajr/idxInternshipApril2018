fe0750e6ae9c6d688535c4b1ef977772745b2920
KadonDEngle <englekadondax@gmail.com>
2018-05-22 16:03:41 -0700

    Update With Font Awesome Lines
    Prepended font awesome lines to head tag in html.

--- wrapperTool/index.js
+++ wrapperTool/index.js
@@ -6,6 +6,7 @@ var site = 'http://dexlinx.valuedlender.com/purl/buyers';
 var divId = '';
 var divClass = '.sub_content';
 var startStop = '<div id="idxStart"></div> <div id="idxStop"></div>';
+var FALines = '<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet"> <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet"/>'
 
 
 axios.get(site)
@@ -13,6 +14,8 @@ axios.get(site)
     var html = response.data;
     var parsed = absolutify(html, site);
     const $ = cheerio.load(parsed);
+    $('head').prepend(FALines);
     $(divClass).text(startStop);
+    console.log($.html());
     return $.html();
 })
